121dad70603b093fcbc8107ae0b868c16fc1809b
davidj24 <david_jo@berkeley.edu>
2025-11-13 09:30:53 -0800

    Added functional live viewer

--- train.py
+++ train.py
@@ -7,13 +7,14 @@ import sys
 import tyro
 from dataclasses import dataclass
 from wandb.integration.sb3 import WandbCallback
-from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.callbacks import BaseCallback, CheckpointCallback
 
 
 @dataclass
 class Args:
-    name_prefix: str = ""
+    name_prefix: str = "m"
     viewer: bool = True
+    wandb_log: bool = True
 
     # Training params
     policy_type: str = "MlpPolicy"
@@ -29,17 +30,20 @@ class Args:
     wandb_project: str = "sb3-lunar-lander"
 
 
-class LiveViewerCallback(CheckpointCallback):
-    def __init__(self, *args, **kwargs):
-        super(LiveViewerCallback, self).__init__(*args, **kwargs)
+class LiveViewerCallback(BaseCallback):
+    def __init__(self, save_freq: int, save_path: str, name_prefix: str, verbose=0):
+        super(LiveViewerCallback, self).__init__(verbose)
+        self.save_freq = save_freq
+        self.save_path = save_path
+        self.name_prefix = name_prefix
         self.viewer_process = None
-        self.newest_path = ""
+        self.log_file = "viewer.log"
 
     def _on_step(self) -> bool:
-        continue_training = super(LiveViewerCallback, self)._on_step()
-        if hasattr(self, 'last_save_path') and self.last_save_path != self.newest_path:
-            print(f"Parent has saved new model to {self.last_save_path}")
-            self.newest_path = self.last_save_path
+        if self.n_calls % self.save_freq == 0:
+            path = os.path.join(self.save_path, f"{self.name_prefix}_{self.n_calls}_steps.zip")
+            self.model.save(path)
+            print(f"Model saved to {path}")
 
             if self.viewer_process and self.viewer_process.poll() is None:
                 print(f"Terminating old viewer")
@@ -50,29 +54,44 @@ class LiveViewerCallback(CheckpointCallback):
                 sys.executable,
                 "viewer.py",
                 "--model-path",
-                self.last_save_path
+                path
             ]
 
-            print(f"Launching new viewer using command: {' '.join(command)}")
-            self.viewer_process = subprocess.Popen(command)
-        return continue_training
+
+            try:
+                print(f"Launching new viewer using command: {' '.join(command)}. Output in {self.log_file}")
+                self.logs = open(self.log_file, "w")
+                self.viewer_process = subprocess.Popen(
+                    command,
+                    stdout=self.logs,
+                    stderr=subprocess.STDOUT
+                )
+            except Exception as e:
+                print(f"Failed to launch viewer. Full error: {e}")
+                self.viewer_process = None
+                
+        return True
     
+
+
     def _on_training_end(self):
-        super(LiveViewerCallback, self)._on_training_end()
         if self.viewer_process and self.viewer_process.poll() is None:
             print("Training has ended. Closing viewer.")
             self.viewer_process.terminate()
             self.viewer_process.wait()
+        if hasattr(self, 'logs'):
+            self.logs.close()
 
 
 
 
 def main(args: Args):
-    run = wandb.init(
-        project="sb3-lunar-lander",
-        config=vars(args),
-        sync_tensorboard=True,
-    )
+    if args.wandb_log:
+        run = wandb.init(
+            project="sb3-lunar-lander",
+            config=vars(args),
+            sync_tensorboard=True,
+        )
 
     print("Creating env")
     env = gym.make(args.env_id)
@@ -80,16 +99,29 @@ def main(args: Args):
 
 
     print("Creating model")
-    model = PPO(
-        args.policy_type,
-        env,
-        learning_rate=args.learning_rate,
-        n_steps=args.n_steps,
-        verbose=1,
-        tensorboard_log=f"wandb_logs/{run.id}"
-    )
+    if args.wandb_log:
+        model = PPO(
+            args.policy_type,
+            env,
+            learning_rate=args.learning_rate,
+            n_steps=args.n_steps,
+            verbose=1,
+            tensorboard_log=f"wandb_logs/{run.id}"
+        )
+    else:
+        model = PPO(
+            args.policy_type,
+            env,
+            learning_rate=args.learning_rate,
+            n_steps=args.n_steps,
+            verbose=1,
+        )
+
     
-    callbacks = [WandbCallback()]
+    callbacks = []
+
+    if args.wandb_log:
+        callbacks.append(WandbCallback())
 
     if args.viewer:
         print(f">>>>>>> Live viewer ENABLED <<<<<<<")
@@ -124,7 +156,8 @@ def main(args: Args):
     )
     print("Training complete.")
 
-    run.finish()
+    if args.wandb_log:
+        run.finish()
     env.close()
 
     print("Run completed and env closed.")
